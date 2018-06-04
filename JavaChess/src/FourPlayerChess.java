a14dc1b6741ec507ac0c235241fb63a7355d43f5
kjw97 <kjw97@10.210.98.119>
2018-06-04 15:38:21 +0900

    java
    Signed-off-by: kjw97 <kjw97@10.210.98.119>

---
+++ JavaChess/src/FourPlayerChess.java
@@ -0,0 +1,19 @@
+
+public abstract class FourPlayerChess implements Rule{
+	public abstract boolean IsCastling ();{
+		if()
+	}
+    public abstract boolean IsPawnPromotion ();{
+		
+	}
+    public abstract boolean IsCheck ();{
+	
+    }
+    public abstract boolean IsCheckMate ();{
+	
+    }
+    public abstract boolean IsStaleMate ();{
+	
+    }
+
+}
---
+++ JavaChess/src/Rule.java
@@ -0,0 +1,9 @@
+
+public interface Rule {
+	boolean IsCastling ();
+	boolean IsPawnPromotion ();
+	boolean IsCheck();
+	boolean IsCheckMate();
+	boolean IsStaleMate();
+	
+}
